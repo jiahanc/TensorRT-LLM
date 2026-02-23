a5729ad02447f0272b0e3ec0de0a18e1dd6a7394
Siyuan Fu <siyuanf@nvidia.com>
2026-02-23 23:14:20 +0000

    add silu
    Signed-off-by: Siyuan Fu <siyuanf@nvidia.com>

--- tests/unittest/_torch/thop/serial/test_moe.py
+++ tests/unittest/_torch/thop/serial/test_moe.py
@@ -37,6 +37,7 @@ from tensorrt_llm.quantization.utils.fp4_utils import (
 class ActType(Enum):
     SwiGlu = 0
     Relu2 = 1
+    Silu = 2
 
 
 class moe_args:
@@ -422,6 +423,8 @@ def run_moe_dequant(args,
             activation_output[i:i + my_num_tokens] = act * (beta + my_x1)
         elif args.act_type == ActType.Relu2:
             activation_output[i:i + my_num_tokens] = F.relu(my_x1)**2
+        elif args.act_type == ActType.Silu:
+            activation_output[i:i + my_num_tokens] = F.silu(my_x1)
         i += my_num_tokens
         i = (i + args.padding - 1) // args.padding * args.padding
 
@@ -1021,8 +1024,9 @@ class TestMoeFp4:
     @pytest.mark.parametrize("num_tokens", [1, 1024])
     @pytest.mark.parametrize("hidden_size", [1024])
     @pytest.mark.parametrize("intermediate_size", [1024, 768])
-    @pytest.mark.parametrize("act_type", [ActType.SwiGlu, ActType.Relu2],
-                             ids=["swiglu", "relu2"])
+    @pytest.mark.parametrize("act_type",
+                             [ActType.SwiGlu, ActType.Relu2, ActType.Silu],
+                             ids=["swiglu", "relu2", "silu"])
     @pytest.mark.parametrize(
         "routing_info",
         [
@@ -1148,8 +1152,9 @@ class TestMoeFp4:
     @pytest.mark.parametrize("num_tokens", [1, 150])
     @pytest.mark.parametrize("hidden_size", [1024])
     @pytest.mark.parametrize("intermediate_size", [1024])
-    @pytest.mark.parametrize("act_type", [ActType.SwiGlu, ActType.Relu2],
-                             ids=["swiglu", "relu2"])
+    @pytest.mark.parametrize("act_type",
+                             [ActType.SwiGlu, ActType.Relu2, ActType.Silu],
+                             ids=["swiglu", "relu2", "silu"])
     @pytest.mark.parametrize(
         "routing_info",
         [
@@ -1623,7 +1628,7 @@ class TestMoeFp4:
             scale_c_fc1 = args_dequant.c_global_sf * (
                 1.0 / args.gemm1_scales_global) * (
                     1.0 / args.hidden_states_scale_global)
-        elif act_type == ActType.Relu2:
+        elif act_type in [ActType.Relu2, ActType.Silu]:
             scale_c_fc1 = torch.full_like(args.gemm1_scales_global,
                                           args_dequant.c_global_sf)
         # self.fc31_alpha
@@ -1673,7 +1678,7 @@ class TestMoeFp4:
                 do_finalize=True,
                 topk_ids=topk_ids,
                 topk_weights=topk_weights,
-                act_type=1 if act_type == ActType.Relu2 else 0)
+                act_type=act_type.value)
         torch.cuda.synchronize()
         output_dequant_actual = output[0].to(torch.float)
 
